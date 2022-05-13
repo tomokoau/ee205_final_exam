2714b15ab63e957b31bb793be6f27024e304cbf8
tomokoaustin <tomokoau@hawaii.edu>
2022-05-12 20:24:47 -1000

    just making sure

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Tomoko Austin <tomokoau@hawaii.edu>
+/// @date   12_May_2022
+///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
 int main() {
